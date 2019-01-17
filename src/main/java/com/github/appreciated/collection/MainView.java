17745b1d7e0319fa08e649f5dfce63bd5270f5e5
Johannes Goebel <GoebelJohannes@gmx.net>
2019-01-17 20:38:34 +0100

    fixed some styling issues

--- src/main/java/com/github/appreciated/collection/MainView.java
+++ src/main/java/com/github/appreciated/collection/MainView.java
@@ -4,6 +4,7 @@ import com.github.appreciated.card.RippleClickableCard;
 import com.github.appreciated.card.content.IconItem;
 import com.github.appreciated.collection.demo.card.CardDemo;
 import com.github.appreciated.collection.demo.demohelper.DemoHelperDemoView;
+import com.github.appreciated.collection.demo.ironcollapse.IronCollapseDemo;
 import com.github.appreciated.demo.helper.DemoHelperView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Image;
@@ -22,25 +23,28 @@ public class MainView extends VerticalLayout {
     public MainView() {
         add(new DemoHelperView().withVerticalHeader("Appreciated Collection",
                 "Welcome to the Appreciated Collection",
-                "./frontend/images/no-icon.png"
+                "./frontend/images/no-logo.png"
                 ).withComponent(new HorizontalLayout(
-                        getCard("./frontend/images/no-icon.png", "Demo Helper Addon", "A addon to help you creating beautiful demos in no time", DemoHelperDemoView.class),
-                        getCard("./frontend/images/no-icon.png", "Card Addon", "A addon that brings cards to Lumo", CardDemo.class),
-                        getCard("./frontend/images/icon.png", "Demo Helper Addon", "A addon to help you creating beautiful demos in no time", DemoHelperDemoView.class))
+                        getCard("./frontend/images/no-logo.png", "Demo Helper Addon", "A addon that helps you creating beautiful addon demos in no time", DemoHelperDemoView.class),
+                        getCard("./frontend/images/no-logo.png", "Card Addon", "A addon that brings cards to Vaadin Flow", CardDemo.class),
+                        getCard("./frontend/images/no-logo.png", "Iron Collapse Addon", "A addon that brings Iron Collapse to Vaadin Flow", IronCollapseDemo.class))
                 )
         );
     }
 
     private Component getCard(String imagePath, String title, String description, Class<? extends Component> route) {
         Image img = new Image(imagePath, title);
-        img.setWidth("50px");
-        img.setHeight("50px");
+        img.setWidth("60px");
+        img.setHeight("60px");
+
+        IconItem item = new IconItem(img, title, description);
+
         RippleClickableCard card = new RippleClickableCard(
-                event -> getUI().ifPresent(ui -> ui.navigate(route.getAnnotation(Route.class).value())),
-                new IconItem(img, title, description)
+                event -> getUI().ifPresent(ui -> ui.navigate(route.getAnnotation(Route.class).value())), item
+
         );
-        card.setWidth("300px");
-        card.setHeight("50px");
+        item.setWidth("310px");
+        item.setHeight("131px");
         return card;
     }
 
