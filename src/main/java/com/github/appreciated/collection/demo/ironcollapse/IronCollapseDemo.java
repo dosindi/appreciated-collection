fafc9db597dd4b515c182aa2e57932ec0bba0793
Johannes Goebel <GoebelJohannes@gmx.net>
2019-01-31 12:13:32 +0100

    - updated iron-collapse demo

--- src/main/java/com/github/appreciated/collection/demo/ironcollapse/IronCollapseDemo.java
+++ src/main/java/com/github/appreciated/collection/demo/ironcollapse/IronCollapseDemo.java
@@ -2,14 +2,13 @@ package com.github.appreciated.collection.demo.ironcollapse;
 
 
 import com.github.appreciated.IronCollapse;
-import com.github.appreciated.card.Card;
-import com.github.appreciated.card.content.IconItem;
 import com.github.appreciated.demo.helper.DemoHelperView;
-import com.github.appreciated.demo.helper.view.devices.PhoneView;
+import com.github.appreciated.demo.helper.view.devices.Orientation;
+import com.github.appreciated.demo.helper.view.devices.TabletView;
 import com.github.appreciated.demo.helper.view.entity.CodeExample;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
@@ -21,10 +20,8 @@ public class IronCollapseDemo extends DemoHelperView {
     public IronCollapseDemo() {
         withHorizontalHeader("Iron Collapse",
                 "A Component that allows the user to show and hide Content",
-                "./frontend/images/no-logo.png"
-        ).withDevice(
-                new PhoneView(getExample()),
-                "Try it out!")
+                "./frontend/images/no-logo.png")
+                .withDevice(new TabletView(getExample()).withOrientation(Orientation.PORTRAIT), "Clicking on the Button opens/closes the IronCollapse")
                 .withStep("Add dependency", "Add the dependency by adding it to your POM",
                         new CodeExample("<dependency>\n" +
                                 "    <groupId>com.github.appreciated</groupId>\n" +
@@ -35,38 +32,24 @@ public class IronCollapseDemo extends DemoHelperView {
                         new CodeExample("install", "xml", "Maven")
                 )
                 .resetCounterStep()
-                .withStep("Some code examples", "You can add Iron Collapse to your View the following way",
-                        new CodeExample("", "java", "Java")
+                .withStep("Some code examples", "Add IronCollapse to your View",
+                        new CodeExample("VerticalLayout collapseContent = new VerticalLayout(new Label(\"< Your Content >\"));\n" +
+                                "collapseContent.getStyle().set(\"border\", \"1px solid var(--lumo-primary-color)\");\n" +
+                                "collapseContent.setWidth(\"400px\");\n" +
+                                "collapseContent.setHeight(\"400px\");\n" +
+                                "IronCollapse collapse = new IronCollapse(collapseContent);", "java", "Java")
                 );
     }
 
     private Component getExample() {
-        IronCollapse collapse = new IronCollapse(getExampleCards());
-        collapse.setWidth("100%");
-        return new VerticalLayout(collapse, new Button("Click me!", event -> collapse.toggle()));
-    }
-
-    private Component getExampleCards() {
-        VerticalLayout vl = new VerticalLayout(
-                getCard("./frontend/images/no-logo.png", "A Plain Card", "I contain stuff!"),
-                getCard("./frontend/images/no-logo.png", "A Plain Card", "I contain stuff!"),
-                getCard("./frontend/images/no-logo.png", "A Plain Card", "I contain stuff!")
-        );
-        vl.setMargin(false);
-        vl.setPadding(true);
-        vl.setSizeFull();
-        return vl;
-    }
-
-    private Component getCard(String imagePath, String title, String description) {
-        Image img = new Image(imagePath, title);
-        img.setWidth("50px");
-        img.setHeight("50px");
-        Card card = new Card(
-                new IconItem(img, title, description)
-        );
-        card.setWidth("100%");
-        return card;
+        VerticalLayout collapseContent = new VerticalLayout(new Label("< Your Content >"));
+        collapseContent.getStyle().set("border", "1px solid var(--lumo-primary-color)");
+        collapseContent.setWidth("400px");
+        collapseContent.setHeight("400px");
+        collapseContent.setAlignItems(Alignment.CENTER);
+        collapseContent.setJustifyContentMode(JustifyContentMode.CENTER);
+        IronCollapse collapse = new IronCollapse(collapseContent);
+        return new VerticalLayout(new Button("Click me!", event -> collapse.toggle()), collapse);
     }
 
 }
