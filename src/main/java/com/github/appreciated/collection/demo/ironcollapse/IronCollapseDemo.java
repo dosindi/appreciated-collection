c6cda1ae4db82f013517caf3e60b3a90fa64eb99
Johannes Goebel <GoebelJohannes@gmx.net>
2019-01-31 12:14:10 +0100

    - updated iron-collapse demo

--- src/main/java/com/github/appreciated/collection/demo/ironcollapse/IronCollapseDemo.java
+++ src/main/java/com/github/appreciated/collection/demo/ironcollapse/IronCollapseDemo.java
@@ -19,7 +19,7 @@ public class IronCollapseDemo extends DemoHelperView {
 
     public IronCollapseDemo() {
         withHorizontalHeader("Iron Collapse",
-                "A Component that allows the user to show and hide Content",
+                "A Component that allows to show and hide Content",
                 "./frontend/images/no-logo.png")
                 .withDevice(new TabletView(getExample()).withOrientation(Orientation.PORTRAIT), "Clicking on the Button opens/closes the IronCollapse")
                 .withStep("Add dependency", "Add the dependency by adding it to your POM",
