4367afb864720ea452ef4666655ae012fd1ebd2e
Johannes Goebel <GoebelJohannes@gmx.net>
2019-01-31 12:49:39 +0100

    - updated paper-ripple demo

--- src/main/java/com/github/appreciated/collection/demo/paperripple/PaperRippleDemo.java
+++ src/main/java/com/github/appreciated/collection/demo/paperripple/PaperRippleDemo.java
@@ -1,14 +1,13 @@
 package com.github.appreciated.collection.demo.paperripple;
 
 
+import com.github.appreciated.collection.demo.component.ExampleContent;
 import com.github.appreciated.demo.helper.DemoHelperView;
 import com.github.appreciated.demo.helper.view.devices.Orientation;
 import com.github.appreciated.demo.helper.view.devices.TabletView;
 import com.github.appreciated.demo.helper.view.entity.CodeExample;
-import com.github.appreciated.prism.element.PrismHighlighter;
+import com.github.appreciated.ripple.PaperRippleVerticalLayout;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
 
@@ -17,43 +16,26 @@ import com.vaadin.flow.router.Route;
 public class PaperRippleDemo extends DemoHelperView {
 
     public PaperRippleDemo() {
-        withHorizontalHeader("Prism Ripple",
+        withHorizontalHeader("PaperRipple",
                 "Provides code/syntax highlighting by using prism",
                 "./frontend/images/no-logo.png")
                 .withDevice(new TabletView(getExample()).withOrientation(Orientation.PORTRAIT), "Some highlighting examples")
                 .withStep("Add dependency", "Add the dependency by adding it to your POM",
                         new CodeExample("<dependency>\n" +
-                                "    <groupId>com.github.appreciated</groupId>\n" +
-                                "    <artifactId>prism-element</artifactId>\n" +
-                                "    <version>0.5</version>\n" +
+                                "   <groupId>com.github.appreciated</groupId>\n" +
+                                "   <artifactId>paper-ripple</artifactId>\n" +
+                                "   <version>1.0</version>\n" +
                                 "</dependency>", "xml", "Maven"))
                 .withStep("Install dependency", "Install the dependency by running the following Maven goal",
                         new CodeExample("install", "xml", "Maven")
                 )
                 .resetCounterStep()
-                .withStep("Some code examples", "Add Paper Color to your View",
-                        new CodeExample("PrismHighlighter javaCode = new PrismHighlighter(\"String a = \\\"Test1234\\\";\\n\" +\n" +
-                                "        \"String b = \\\"Test1234\\\";\\n\" +\n" +
-                                "        \"String c = \\\"Test1234\\\";\\n\" +\n" +
-                                "        \"String d = \\\"Test1234\\\";\", \"java\");\n" +
-                                "PrismHighlighter xmlCode = new PrismHighlighter(\"<dependency>\\n\" +\n" +
-                                "        \"    <groupId>com.github.appreciated</groupId>\\n\" +\n" +
-                                "        \"    <artifactId>prism-element</artifactId>\\n\" +\n" +
-                                "        \"    <version>0.5</version>\\n\" +\n" +
-                                "        \"</dependency>\", \"xml\");", "java", "Java")
+                .withStep("Some code examples", "Add PaperRipple to your View",
+                        new CodeExample("return new PaperRippleVerticalLayout(new ExampleContent())", "java", "Java")
                 );
     }
 
     private Component getExample() {
-        PrismHighlighter javaCode = new PrismHighlighter("String a = \"Test1234\";\n" +
-                "String b = \"Test1234\";\n" +
-                "String c = \"Test1234\";\n" +
-                "String d = \"Test1234\";", "java");
-        PrismHighlighter xmlCode = new PrismHighlighter("<dependency>\n" +
-                "    <groupId>com.github.appreciated</groupId>\n" +
-                "    <artifactId>prism-element</artifactId>\n" +
-                "    <version>0.5</version>\n" +
-                "</dependency>", "xml");
-        return new VerticalLayout(new Label("Java Highlighting"), javaCode, new Label("XML Highlighting"), xmlCode);
+        return new PaperRippleVerticalLayout(new ExampleContent());
     }
 }
