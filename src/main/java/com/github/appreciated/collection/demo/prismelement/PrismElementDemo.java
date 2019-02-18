07aab1a5032eb83840a51b545938ffde2b56c2e7
Johannes Goebel <GoebelJohannes@gmx.net>
2019-02-18 16:54:12 +0100

    - updated prism-highlighter example

--- src/main/java/com/github/appreciated/collection/demo/prismelement/PrismElementDemo.java
+++ src/main/java/com/github/appreciated/collection/demo/prismelement/PrismElementDemo.java
@@ -1,16 +1,13 @@
 package com.github.appreciated.collection.demo.prismelement;
 
 
+import com.github.appreciated.collection.demo.prismelement.demo.PrismHighlighterExample;
 import com.github.appreciated.collection.maven.AppreciatedDependencyReader;
 import com.github.appreciated.demo.helper.DemoHelperView;
 import com.github.appreciated.demo.helper.view.devices.Orientation;
 import com.github.appreciated.demo.helper.view.devices.TabletView;
 import com.github.appreciated.demo.helper.view.entity.CodeExample;
 import com.github.appreciated.prism.element.Language;
-import com.github.appreciated.prism.element.PrismHighlighter;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.router.Route;
 
@@ -22,36 +19,15 @@ public class PrismElementDemo extends DemoHelperView {
         withHorizontalHeader("Prism Element",
                 "Provides code/syntax highlighting by using prism",
                 "./frontend/images/no-logo.png")
-                .withDevice(new TabletView(getExample()).withOrientation(Orientation.PORTRAIT), "Some highlighting examples")
+                .withDevice(new TabletView(new PrismHighlighterExample()).withOrientation(Orientation.PORTRAIT), "Some highlighting examples")
                 .withStep("Add dependency", "Add the dependency by adding it to your POM",
                         new CodeExample(new AppreciatedDependencyReader("prism-element").getDependencyString(), Language.markup, "Maven"))
                 .withStep("Install dependency", "Install the dependency by running the following Maven goal",
                         new CodeExample("install", Language.markup, "Maven")
                 )
                 .resetCounterStep()
-                .withStep("Some code examples", "Add Paper Color to your View",
-                        new CodeExample("PrismHighlighter javaCode = new PrismHighlighter(\"String a = \\\"Test1234\\\";\\n\" +\n" +
-                                "        \"String b = \\\"Test1234\\\";\\n\" +\n" +
-                                "        \"String c = \\\"Test1234\\\";\\n\" +\n" +
-                                "        \"String d = \\\"Test1234\\\";\", \"java\");\n" +
-                                "PrismHighlighter xmlCode = new PrismHighlighter(\"<dependency>\\n\" +\n" +
-                                "        \"    <groupId>com.github.appreciated</groupId>\\n\" +\n" +
-                                "        \"    <artifactId>prism-element</artifactId>\\n\" +\n" +
-                                "        \"    <version>0.5</version>\\n\" +\n" +
-                                "        \"</dependency>\", \"xml\");", Language.java, "Java")
+                .withStep("Some code examples", "Add PrismHighlighter to your View",
+                        new CodeExample(PrismHighlighterExample.class)
                 );
-    }
-
-    private Component getExample() {
-        PrismHighlighter javaCode = new PrismHighlighter("String a = \"Test1234\";\n" +
-                "String b = \"Test1234\";\n" +
-                "String c = \"Test1234\";\n" +
-                "String d = \"Test1234\";", Language.java);
-        PrismHighlighter xmlCode = new PrismHighlighter("<dependency>\n" +
-                "    <groupId>com.github.appreciated</groupId>\n" +
-                "    <artifactId>prism-element</artifactId>\n" +
-                "    <version>0.5</version>\n" +
-                "</dependency>", Language.markup);
-        return new VerticalLayout(new Label("Java Highlighting"), javaCode, new Label("XML Highlighting"), xmlCode);
     }
 }
---
+++ src/main/java/com/github/appreciated/collection/demo/prismelement/demo/PrismHighlighterExample.java
@@ -0,0 +1,23 @@
+package com.github.appreciated.collection.demo.prismelement.demo;
+
+import com.github.appreciated.prism.element.Language;
+import com.github.appreciated.prism.element.PrismHighlighter;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class PrismHighlighterExample extends VerticalLayout {
+    public PrismHighlighterExample() {
+        PrismHighlighter javaCode = new PrismHighlighter(" String a = \"Test1234\";\n" +
+                "        String b = \"Test1234\";\n" +
+                "        String c = \"Test1234\";\n" +
+                "        String d = \"Test1234\";", Language.java);
+
+        PrismHighlighter xml = new PrismHighlighter("<dependency>\n" +
+                "    <groupId>com.github.appreciated</groupId>\n" +
+                "    <artifactId>prism-element</artifactId>\n" +
+                "    <version>0.7</version>\n" +
+                "</dependency>", Language.markup);
+
+        add(new Label("Java Highlighting"), javaCode, new Label("XML Highlighting"), xml);
+    }
+}
