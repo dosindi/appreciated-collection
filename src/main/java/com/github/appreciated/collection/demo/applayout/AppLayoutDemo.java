2049654dc09befae7fb2918baa1fc99a041dd510
Johannes Goebel <GoebelJohannes@gmx.net>
2019-02-18 17:58:59 +0100

    - replaced embed with Iframe

--- src/main/java/com/github/appreciated/collection/demo/applayout/AppLayoutDemo.java
+++ src/main/java/com/github/appreciated/collection/demo/applayout/AppLayoutDemo.java
@@ -1,13 +1,13 @@
 package com.github.appreciated.collection.demo.applayout;
 
 
-import com.github.appreciated.collection.component.Embed;
 import com.github.appreciated.collection.demo.applayout.demo.View1;
 import com.github.appreciated.collection.demo.applayout.demo.View2;
 import com.github.appreciated.collection.demo.applayout.demo.YourAppLayoutRouterLayout;
 import com.github.appreciated.collection.demo.applayout.views.MainView;
 import com.github.appreciated.collection.maven.AppreciatedDependencyReader;
 import com.github.appreciated.demo.helper.DemoHelperView;
+import com.github.appreciated.demo.helper.component.IFrame;
 import com.github.appreciated.demo.helper.view.devices.DeviceType;
 import com.github.appreciated.demo.helper.view.devices.PhoneView;
 import com.github.appreciated.demo.helper.view.entity.CodeExample;
@@ -21,9 +21,9 @@ public class AppLayoutDemo extends DemoHelperView {
 
     public AppLayoutDemo() {
         withHorizontalHeader("AppLayout", "A responsive layout that allows you to create modern Menus quickly", "./frontend/images/applayout/app-layout-logo.png")
-                .withDevice(new PhoneView(new Embed(MainView.class).withFullSize()), "Optimized for small screens")
+                .withDevice(new PhoneView(new IFrame(MainView.class).withFullSize()), "Optimized for small screens")
                 .withParagraph("Behaviours", "The AppLayout has by default multiple Behaviours availabe. Try to set them via the menu in the demo above and below")
-                .withStylableDevice(new Embed(MainView.class).withFullSize(), DeviceType.TABLET_LANDSCAPE)
+                .withStylableDevice(new IFrame(MainView.class).withFullSize(), DeviceType.TABLET_LANDSCAPE)
                 .withStep("Add dependency", "Add the dependency by adding it to your POM",
                         new CodeExample(new AppreciatedDependencyReader("app-layout-addon").getDependencyString(), Language.markup, "Maven"))
                 .withStep("Install dependency", "Install the dependency by running the following Maven goal",
