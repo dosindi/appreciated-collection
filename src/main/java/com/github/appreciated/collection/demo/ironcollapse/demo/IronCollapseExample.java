72fb82573c011911bde40015632f96cc64692f9a
Johannes Goebel <GoebelJohannes@gmx.net>
2019-03-25 12:02:03 +0100

    updated iron-collapse demo

--- src/main/java/com/github/appreciated/collection/demo/ironcollapse/demo/IronCollapseExample.java
+++ src/main/java/com/github/appreciated/collection/demo/ironcollapse/demo/IronCollapseExample.java
@@ -3,6 +3,7 @@ package com.github.appreciated.collection.demo.ironcollapse.demo;
 import com.github.appreciated.IronCollapse;
 import com.github.appreciated.IronCollapseLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -21,8 +22,10 @@ public class IronCollapseExample extends VerticalLayout {
         IronCollapse collapse = new IronCollapse(collapseContent);
         add(collapse);
         add(new H2("Server-Sided"),
-                new Button("Click me!", event -> collapse.toggle()),
-                collapse,
+                new Div( // Better wrap inside a div, leads other wise to some weird effects due to v/h-layout margins
+                        new Button("Click me!", event -> collapse.toggle()),
+                        collapse
+                ),
                 new Label("Some Content below")
         );
     }
