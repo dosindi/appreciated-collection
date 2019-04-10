32941f7f7bf498ead0a97b5919bfe20bb9cb0a73
Johannes Goebel <GoebelJohannes@gmx.net>
2019-04-11 01:17:03 +0200

    updated apex charts dependency

--- src/main/java/com/github/appreciated/collection/demo/apexcharts/demo/ApexChartsExample.java
+++ src/main/java/com/github/appreciated/collection/demo/apexcharts/demo/ApexChartsExample.java
@@ -1,32 +1,44 @@
 package com.github.appreciated.collection.demo.apexcharts.demo;
 
 import com.github.appreciated.apexcharts.ApexCharts;
-import com.github.appreciated.apexcharts.config.Chart;
 import com.github.appreciated.apexcharts.config.builder.*;
 import com.github.appreciated.apexcharts.config.chart.Type;
 import com.github.appreciated.apexcharts.config.chart.zoom.builder.ZoomBuilder;
 import com.github.appreciated.apexcharts.config.grid.builder.RowBuilder;
+import com.github.appreciated.apexcharts.config.legend.HorizontalAlign;
 import com.github.appreciated.apexcharts.config.stroke.Curve;
 import com.github.appreciated.apexcharts.config.subtitle.Align;
-import com.github.appreciated.apexcharts.helper.MapSeries;
+import com.github.appreciated.apexcharts.helper.Series;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
 
 public class ApexChartsExample extends VerticalLayout {
     public ApexChartsExample() {
         add(getPieChart());
+        add(getDonutChart());
         add(getLineChart());
+        add(getAreaChart());
+        getStyle().set("overflow", "auto");
     }
 
     private Component getPieChart() {
-        Chart chart = new Chart();
-        chart.setType(Type.pie);
-
         ApexCharts pieChart = new ApexCharts()
-                .withChart(chart)
+                .withChart(ChartBuilder.get().withType(Type.pie).build())
                 .withLabels("Team A", "Team B", "Team C", "Team D", "Team E")
                 .withSeries(44.0, 55.0, 13.0, 43.0, 22.0);
+        pieChart.setHeight("300px");
         return pieChart;
+    }
+
+    private Component getDonutChart() {
+        ApexCharts donutChart = new ApexCharts()
+                .withChart(ChartBuilder.get().withType(Type.donut).build())
+                .withSeries(44.0, 55.0, 41.0, 17.0, 15.0);
+        donutChart.setHeight("300px");
+        return donutChart;
     }
 
     private Component getLineChart() {
@@ -52,9 +64,40 @@ public class ApexChartsExample extends VerticalLayout {
                 .withXaxis(XAxisBuilder.get()
                         .withCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep")
                         .build())
-                .withSeries(new MapSeries("Desktops", 10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0));
-        lineChart.setWidth("550px");
-        lineChart.setHeight("350px");
+                .withSeries(new Series("Desktops", 10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0));
+        lineChart.setWidth("500px");
+        lineChart.setHeight("300px");
         return lineChart;
+    }
+
+    private Component getAreaChart() {
+        ApexCharts areaChart = new ApexCharts()
+                .withChart(
+                        ChartBuilder.get()
+                                .withType(Type.area)
+                                .withZoom(ZoomBuilder.get()
+                                        .withEnabled(false)
+                                        .build())
+                                .build())
+                .withDataLabels(DataLabelsBuilder.get()
+                        .withEnabled(false)
+                        .build())
+                .withStroke(StrokeBuilder.get().withCurve(Curve.straight).build())
+                .withSeries(new Series("STOCK ABC", 10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0))
+                .withTitle(TitleSubtitleBuilder.get()
+                        .withText("Fundamental Analysis of Stocks")
+                        .withAlign(Align.left).build())
+                .withSubtitle(TitleSubtitleBuilder.get()
+                        .withText("Price Movements")
+                        .withAlign(Align.left).build())
+                .withLabels(IntStream.range(1, 10).boxed().map(day -> LocalDate.of(2000, 1, day).toString()).toArray(String[]::new))
+                .withXaxis(XAxisBuilder.get()
+                        .withType(com.github.appreciated.apexcharts.config.xaxis.Type.datetime).build())
+                .withYaxis(YAxisBuilder.get()
+                        .withOpposite(true).build())
+                .withLegend(LegendBuilder.get().withHorizontalAlign(HorizontalAlign.left).build());
+        areaChart.setWidth("500px");
+        areaChart.setHeight("300px");
+        return areaChart;
     }
 }
