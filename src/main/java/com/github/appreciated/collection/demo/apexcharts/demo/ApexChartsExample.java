1301109bdfa428ffaff98fdd6d0587e3f4a0ad04
Johannes Goebel <GoebelJohannes@gmx.net>
2019-04-11 10:58:36 +0200

    added bar chart example

--- src/main/java/com/github/appreciated/collection/demo/apexcharts/demo/ApexChartsExample.java
+++ src/main/java/com/github/appreciated/collection/demo/apexcharts/demo/ApexChartsExample.java
@@ -6,8 +6,10 @@ import com.github.appreciated.apexcharts.config.chart.Type;
 import com.github.appreciated.apexcharts.config.chart.zoom.builder.ZoomBuilder;
 import com.github.appreciated.apexcharts.config.grid.builder.RowBuilder;
 import com.github.appreciated.apexcharts.config.legend.HorizontalAlign;
+import com.github.appreciated.apexcharts.config.plotoptions.builder.BarBuilder;
 import com.github.appreciated.apexcharts.config.stroke.Curve;
 import com.github.appreciated.apexcharts.config.subtitle.Align;
+import com.github.appreciated.apexcharts.config.yaxis.builder.TitleBuilder;
 import com.github.appreciated.apexcharts.helper.Series;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -21,6 +23,7 @@ public class ApexChartsExample extends VerticalLayout {
         add(getDonutChart());
         add(getLineChart());
         add(getAreaChart());
+        add(getBarChart());
         getStyle().set("overflow", "auto");
     }
 
@@ -99,5 +102,39 @@ public class ApexChartsExample extends VerticalLayout {
         areaChart.setWidth("500px");
         areaChart.setHeight("300px");
         return areaChart;
+    }
+
+    private Component getBarChart() {
+        ApexCharts barChart = new ApexCharts()
+                .withChart(ChartBuilder.get()
+                        .withType(Type.bar)
+                        .build())
+                .withPlotOptions(PlotOptionsBuilder.get()
+                        .withBar(BarBuilder.get()
+                                .withHorizontal(false)
+                                .withColumnWidth("55%")
+                                .build())
+                        .build())
+                .withDataLabels(DataLabelsBuilder.get()
+                        .withEnabled(false).build())
+                .withStroke(StrokeBuilder.get()
+                        .withShow(true)
+                        .withWidth(2.0)
+                        .withColors("transparent")
+                        .build())
+                .withSeries(new Series("Net Profit", 44.0, 55.0, 57.0, 56.0, 61.0, 58.0, 63.0, 60.0, 66.0),
+                        new Series("Revenue", 76.0, 85.0, 101.0, 98.0, 87.0, 105.0, 91.0, 114.0, 94.0),
+                        new Series("Free Cash Flow", 35.0, 41.0, 36.0, 26.0, 45.0, 48.0, 52.0, 53.0, 41.0))
+                .withYaxis(YAxisBuilder.get()
+                        .withTitle(TitleBuilder.get()
+                                .withText("$ (thousands)")
+                                .build())
+                        .build())
+                .withXaxis(XAxisBuilder.get().withCategories("Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct").build())
+                .withFill(FillBuilder.get()
+                        .withOpacity(1.0).build());
+        barChart.setWidth("550px");
+        barChart.setHeight("350px");
+        return barChart;
     }
 }
