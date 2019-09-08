f62066d1780479a3f82e0a81aa1c23dbf5a1a5d9
Johannes Goebel <GoebelJohannes@gmx.net>
2019-09-08 22:51:16 +0200

    updated package.json

---
+++ src/main/java/com/github/appreciated/collection/demo/swiper/SwiperDemo.java
@@ -0,0 +1,37 @@
+package com.github.appreciated.collection.demo.swiper;
+
+
+import com.github.appreciated.collection.demo.swiper.demo.SwiperExample;
+import com.github.appreciated.collection.maven.AppreciatedDependencyReader;
+import com.github.appreciated.demo.helper.DemoHelperView;
+import com.github.appreciated.demo.helper.entity.CodeExample;
+import com.github.appreciated.demo.helper.entity.GithubDependencies;
+import com.github.appreciated.demo.helper.entity.GithubUrl;
+import com.github.appreciated.demo.helper.view.devices.Orientation;
+import com.github.appreciated.demo.helper.view.devices.TabletView;
+import com.github.appreciated.prism.element.Language;
+import com.vaadin.flow.component.page.BodySize;
+import com.vaadin.flow.router.Route;
+
+@Route("paper-ripple/")
+@BodySize(height = "100%", width = "100%")
+public class SwiperDemo extends DemoHelperView {
+
+    public SwiperDemo() {
+        super(new GithubUrl("https://github.com/appreciated/paper-ripple"), new GithubDependencies("https://github.com/PolymerElements/paper-ripple"));
+        withHorizontalHeader("PaperRipple",
+                "Allows to give the user feedback when a component was clicked.",
+                "./frontend/images/ripple/logo.png")
+                .withDevice(new TabletView(new SwiperExample()).withOrientation(Orientation.PORTRAIT))
+                .withStep("Add dependency", "Add the dependency by adding it to your POM",
+                        new CodeExample(new AppreciatedDependencyReader("paper-ripple")))
+                .withStep("Install dependency", "Install the dependency by running the following Maven goal",
+                        new CodeExample("install", Language.markup, "Maven")
+                )
+                .resetCounterStep()
+                .withStep("Some code examples", "Add PaperRipple to your View",
+                        new CodeExample(SwiperExample.class)
+                )
+                .withDependencyNotice();
+    }
+}
