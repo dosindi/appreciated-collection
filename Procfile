f87665473cac5c37ecad649896da4e5cf8beeb09
Johannes Goebel <GoebelJohannes@gmx.net>
2019-09-23 14:50:19 +0200

    moved to jetty

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: mvn jetty:run-war -Pproduction $PORT
--- src/main/java/com/github/appreciated/collection/MainView.java
+++ src/main/java/com/github/appreciated/collection/MainView.java
@@ -38,33 +38,37 @@ import com.vaadin.flow.server.PWA;
 public class MainView extends DemoHelperView {
 
     public MainView() {
-        FlexibleGridLayout layout = new FlexibleGridLayout()
-                .withColumns(Repeat.RepeatMode.AUTO_FILL, new MinMax(new Length("320px"), new Flex(1)))
-                .withAutoRows(new Length("131px"))
-                .withItems(
-                        getCard("./frontend/images/applayout/logo.png", "App Layout Addon", "A maddon that allows you building responsive applications easily", AppLayoutDemo.class),
-                        getCard("./frontend/images/no-logo.png", "ApexCharts.js Addon", "An ApexCharts.js wrapper to build interactive charts and visualizations", ApexChartsDemo.class),
-                        getCard("./frontend/images/no-logo.png", "Swiper Addon", "A component to build horizontally and vertically scrollable containers with stops", SwiperDemo.class),
-                        getCard("./frontend/images/demohelper/logo.png", "Demo Helper Addon", "An addon that helps you creating beautiful addon demos in no time", DemoHelperViewDemo.class),
-                        getCard("./frontend/images/gridlayout/logo.png", "Css Grid Layout Addon", "An addon that brings css grid to Vaadin Flow", GridLayoutDemo.class),
-                        getCard("./frontend/images/card/logo.png", "Card Addon", "An addon that brings cards to Vaadin Flow", CardDemo.class),
-                        getCard("./frontend/images/masterdetail/logo.png", "Master/Detail View Addon", "An addon that brings the Master/Detail Pattern to Vaadin Flow", MasterDetailDemo.class),
-                        getCard("./frontend/images/menubutton/logo.png", "PaperMenuButton", "An addon that can open a popup on click", PaperMenuButtonDemo.class),
-                        getCard("./frontend/images/ripple/logo.png", "PaperRipple", "Gives feedback if an area was clicked", PaperRippleDemo.class),
-                        getCard("./frontend/images/prism/logo.png", "Prism-Element Addon", "A code/syntax highlighter addon for Vaadin Flow", PrismElementDemo.class),
-                        getCard("./frontend/images/ironcollapse/logo.png", "Iron Collapse Addon", "An addon that brings Iron Collapse to Vaadin Flow", IronCollapseDemo.class),
-                        getCard("./frontend/images/colorhelper/logo.png", "Calculated Color Helper Addon", "An addon that brings control over the css variables in Vaadin Flow", CalculatedColorHelperDemo.class)
-                )
-                .withPadding(true)
-                .withSpacing(true)
-                .withAutoFlow(GridLayoutComponent.AutoFlow.ROW_DENSE)
-                .withOverflow(GridLayoutComponent.Overflow.AUTO);
-        layout.setWidth("100%");
+        try {
+            FlexibleGridLayout layout = new FlexibleGridLayout()
+                    .withColumns(Repeat.RepeatMode.AUTO_FILL, new MinMax(new Length("320px"), new Flex(1)))
+                    .withAutoRows(new Length("131px"))
+                    .withItems(
+                            getCard("./frontend/images/applayout/logo.png", "App Layout Addon", "A maddon that allows you building responsive applications easily", AppLayoutDemo.class),
+                            getCard("./frontend/images/no-logo.png", "ApexCharts.js Addon", "An ApexCharts.js wrapper to build interactive charts and visualizations", ApexChartsDemo.class),
+                            getCard("./frontend/images/no-logo.png", "Swiper Addon", "A component to build horizontally and vertically scrollable containers with stops", SwiperDemo.class),
+                            getCard("./frontend/images/demohelper/logo.png", "Demo Helper Addon", "An addon that helps you creating beautiful addon demos in no time", DemoHelperViewDemo.class),
+                            getCard("./frontend/images/gridlayout/logo.png", "Css Grid Layout Addon", "An addon that brings css grid to Vaadin Flow", GridLayoutDemo.class),
+                            getCard("./frontend/images/card/logo.png", "Card Addon", "An addon that brings cards to Vaadin Flow", CardDemo.class),
+                            getCard("./frontend/images/masterdetail/logo.png", "Master/Detail View Addon", "An addon that brings the Master/Detail Pattern to Vaadin Flow", MasterDetailDemo.class),
+                            getCard("./frontend/images/menubutton/logo.png", "PaperMenuButton", "An addon that can open a popup on click", PaperMenuButtonDemo.class),
+                            getCard("./frontend/images/ripple/logo.png", "PaperRipple", "Gives feedback if an area was clicked", PaperRippleDemo.class),
+                            getCard("./frontend/images/prism/logo.png", "Prism-Element Addon", "A code/syntax highlighter addon for Vaadin Flow", PrismElementDemo.class),
+                            getCard("./frontend/images/ironcollapse/logo.png", "Iron Collapse Addon", "An addon that brings Iron Collapse to Vaadin Flow", IronCollapseDemo.class),
+                            getCard("./frontend/images/colorhelper/logo.png", "Calculated Color Helper Addon", "An addon that brings control over the css variables in Vaadin Flow", CalculatedColorHelperDemo.class)
+                    )
+                    .withPadding(true)
+                    .withSpacing(true)
+                    .withAutoFlow(GridLayoutComponent.AutoFlow.ROW_DENSE)
+                    .withOverflow(GridLayoutComponent.Overflow.AUTO);
+            layout.setWidth("100%");
+            withVerticalHeader("Appreciated Collection",
+                    "Welcome to the Appreciated Collection",
+                    "./frontend/images/collection/logo.png")
+                    .with(layout);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
 
-        withVerticalHeader("Appreciated Collection",
-                "Welcome to the Appreciated Collection",
-                "./frontend/images/collection/logo.png")
-                .with(layout);
     }
 
     private Component getCard(String imagePath, String title, String description, Class<? extends Component> route) {
